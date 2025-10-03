ddd389a9b2c1ca669a0758232502ed0e1fcead96
luisito343 <luisito343nava@gmail.com>
2025-10-02 22:11:24 -0600

    correcion 508

--- gateway/start.sh
+++ gateway/start.sh
@@ -1,3 +1,6 @@
+#!/bin/sh
+
+# Sustituye todas las variables en nginx.conf.template
 envsubst '
 ${CREATE_STARTUP_URL}
 ${READ_STARTUP_URL}
@@ -9,5 +12,8 @@ ${UPDATE_TECHNOLOGY_URL}
 ${DELETE_TECHNOLOGY_URL}
 ' < /etc/nginx/nginx.conf.template > /etc/nginx/conf.d/default.conf
 
-# Inicia Nginx en primer plano
+echo "✅ Nginx config generada con variables sustituidas:"
+cat /etc/nginx/conf.d/default.conf
+
+# Inicia Nginx en foreground (para que el contenedor no muera)
 nginx -g 'daemon off;'
