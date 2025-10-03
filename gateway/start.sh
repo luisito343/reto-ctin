2aea6e2bebaa2ec969b3ad08b7e77e5f3e62ebcd
luisito343 <luisito343nava@gmail.com>
2025-10-02 23:15:06 -0600

    error 508

--- gateway/start.sh
+++ gateway/start.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# Sustituye todas las variables en nginx.conf.template
+# Sustituye variables en el template
 envsubst '
 ${CREATE_STARTUP_URL}
 ${READ_STARTUP_URL}
@@ -12,8 +12,9 @@ ${UPDATE_TECHNOLOGY_URL}
 ${DELETE_TECHNOLOGY_URL}
 ' < /etc/nginx/nginx.conf.template > /etc/nginx/conf.d/default.conf
 
-echo "✅ Nginx config generada con variables sustituidas:"
+echo "✅ Configuración de Nginx generada:"
 cat /etc/nginx/conf.d/default.conf
 
-# Inicia Nginx en foreground (para que el contenedor no muera)
+# Inicia Nginx
 nginx -g 'daemon off;'
+
